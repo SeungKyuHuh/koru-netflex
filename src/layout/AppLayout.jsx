d6329fec770dc9e2f38d7837497be9546409700d
HuhSeungKyu <l1o1s1e1@hanmail.net>
2026-04-11 01:06:29 +0900

    href -> Link로 변경

--- src/layout/AppLayout.jsx
+++ src/layout/AppLayout.jsx
@@ -5,13 +5,14 @@ import Form from 'react-bootstrap/Form';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const AppLayout = () => {
   return (
     <div>
     <Navbar expand="lg" className="bg-dark navbar-dark">
       <Container fluid>
-        <Navbar.Brand href="/">
+        <Navbar.Brand as={Link} to="/">
         <img
             src="https://images.ctfassets.net/4cd45et68cgf/4nBnsuPq03diC5eHXnQYx/d48a4664cdc48b6065b0be2d0c7bc388/Netflix-Logo.jpg"
             alt="logo"
@@ -25,8 +26,8 @@ const AppLayout = () => {
             style={{ maxHeight: '100px' }}
             navbarScroll
           >
-            <Nav.Link href="/">Home</Nav.Link>
-            <Nav.Link href="/movies">Movies</Nav.Link>
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/movies">Movies</Nav.Link>
           </Nav>
           <Form className="d-flex">
             <Form.Control
