a5ab52562717975475069eb4cb27555ea234169c
HuhSeungKyu <l1o1s1e1@hanmail.net>
2026-04-18 01:08:32 +0900

    path 수정

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 const queryClient = new QueryClient();
 createRoot(document.getElementById('root')).render(  
   <QueryClientProvider client={queryClient}>
-    <BrowserRouter basename="/koru-netflex">
+    <BrowserRouter>
       <App />
     </BrowserRouter>
   </QueryClientProvider>
